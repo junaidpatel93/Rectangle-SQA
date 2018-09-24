180e6451a9a118530dda861ceaf609e87f70fa94
Junaid Patel <Mpatel3787@conestogac.on.ca>
2018-09-24 15:18:43 -0400

    initial commit

---
+++ RectangleSQA/Assignment01_Tests/Assignment01_Tests.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RectangleSQA;
+using NUnit.Framework;
+
+namespace Assignment01_Tests
+{
+    [TestFixture]
+    class Assignment01_Tests
+    {
+        [Test]
+        public void GetLength_input3_expectLengthEquals3()
+        {
+            //Arrange
+            int l = 3;
+            int w = 4;
+            Rectangle rect = new Rectangle(l, w);
+
+            //Act
+            int length = rect.GetLength();
+
+            //Assert
+            Assert.AreEqual(length, l);
+        }
+
+        [Test]
+        public void SetLength_userInput_ExpectChangedValue()
+        {
+            //Arrange
+            int l = 3;
+            int w = 4;
+            Rectangle rect = new Rectangle(l, w);
+
+            //Act
+            int length = rect.SetLength(5);
+
+            //Assert
+            Assert.AreNotEqual(length, l);
+
+        }
+
+        [Test]
+        public void GetWidth_input3_expectWidthEquals3()
+        {
+            //Arrange
+            int l = 4;
+            int w = 3;
+            Rectangle rect = new Rectangle(l, w);
+
+            //Act
+            int width = rect.GetWidth();
+
+            //Assert
+            Assert.AreEqual(width, w);
+        }
+
+        [Test]
+        public void SetWidth_userInput_ExpectChangedValue()
+        {
+            //Arrange
+            int l = 3;
+            int w = 4;
+            Rectangle rect = new Rectangle(l, w);
+
+            //Act
+            int width = rect.SetWidth(5);
+
+            //Assert
+            Assert.AreNotEqual(width, w);
+
+        }
+
+        [Test]
+        public void GetPerimeter_calculatedValue_expectedValue14()
+        {
+            //Arrange
+            int l = 3;
+            int w = 4;
+            Rectangle rect = new Rectangle(l, w);
+
+            //Act
+            int perimeter = rect.GetPerimeter();
+
+            //Assert
+            Assert.AreEqual(perimeter, 2 * (l + w));
+        }
+
+        [Test]
+        public void GetArea_calculatedValue_expectedValue12()
+        {
+            //Arrange
+            int l = 3;
+            int w = 4;
+            Rectangle rect = new Rectangle(l, w);
+
+            //Act
+            int area = rect.GetArea();
+
+            //Assert
+            Assert.AreEqual(area, l * w);
+        }
+    }
+}
---
+++ RectangleSQA/RectangleSQA/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RectangleSQA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.SelectOption();
+            Console.ReadKey();
+
+        }
+        public void SetValues()
+        {
+            Rectangle rec;
+            int len;
+            do
+            {
+                Console.WriteLine("Please enter the length");
+            } while (!int.TryParse(Console.ReadLine(), out len) || len <= 0);
+            //{ Console.WriteLine("The value has to be a positive integer, please enter again."); }
+            int wid;
+            do
+            {
+                Console.WriteLine("Please enter the width");
+            } while (!int.TryParse(Console.ReadLine(), out wid) || wid <= 0);
+            //{ Console.WriteLine("The value has to be a positive integer, please enter again."); }
+            rec = new Rectangle(len, wid);
+        }
+        public void SelectOption()
+        {
+            Rectangle rec;
+            int length;
+            do
+            {
+                Console.WriteLine("Please enter the length");
+            } while (!int.TryParse(Console.ReadLine(), out length) || length <= 0);
+            //{ Console.WriteLine("The value has to be a positive integer, please enter again."); }
+            int width;
+            do
+            {
+                Console.WriteLine("Please enter the width");
+            } while (!int.TryParse(Console.ReadLine(), out width) || width <= 0);
+            //{ Console.WriteLine("The value has to be a positive integer, please enter again."); }
+            rec = new Rectangle(length, width);
+
+            int exitSelect = 0;
+            do
+            {
+                int selection = 0;
+                do
+                {
+                    Console.WriteLine("Please select one option from following options");
+                    Console.WriteLine("1. Get Rectangle Length");
+                    Console.WriteLine("2. Change Rectangle Length");
+                    Console.WriteLine("3. Get Rectangle Width");
+                    Console.WriteLine("4. Change Rectangle Width");
+                    Console.WriteLine("5. Get Rectangle Perimeter");
+                    Console.WriteLine("6. Get Rectangle Area");
+                    Console.WriteLine("7. Exit");
+                } while (!int.TryParse(Console.ReadLine(), out selection) || selection <= 0 || selection > 7);
+                //{ Console.WriteLine("Invalid input, please select from the given numbers."); }
+                exitSelect = selection;
+
+                if (selection == 1)
+                {
+                    Console.WriteLine("The length is {0}", rec.GetLength());
+                }
+                if (selection == 2)
+                {
+                    int len;
+                    do
+                    {
+                        Console.WriteLine("Please enter the length");
+                    } while (!int.TryParse(Console.ReadLine(), out len) || len < 1);
+                    //{ Console.WriteLine("The value has to be a positive integer and greater than 1, please enter again."); }
+                    rec.SetLength(len);
+                    Console.WriteLine("The length has been set as {0}", rec.Length);
+                }
+                if (selection == 3)
+                {
+                    rec = new Rectangle();
+                    Console.WriteLine("The width is {0}", rec.GetWidth());
+                }
+                if (selection == 4)
+                {
+                    int wid = 0;
+                    do
+                    {
+                        Console.WriteLine("Please enter the width");
+                        
+                    } while (!int.TryParse(Console.ReadLine(), out wid) || wid < 1);
+                    //{ Console.WriteLine("The value has to be a positive integer and greater than 1, please enter again."); }
+                    rec.SetWidth(wid);
+                    Console.WriteLine("The width has been set as {0}", rec.Width);
+                }
+                if (selection == 5)
+                {
+                    Console.WriteLine("The perimeter of the rectangle is {0}", rec.GetPerimeter());
+                }
+                if (selection == 6)
+                {
+                    Console.WriteLine("The area of the rectangle is {0}", rec.GetArea());
+                }
+                else if(selection == 7)
+                {
+                    Console.WriteLine("Thank you, have a nice day");
+                }
+            } while (exitSelect != 7);
+            
+        }
+    }
+}
